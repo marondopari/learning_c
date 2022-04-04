c7169068bb010ae8816b42ae7d7f8098604f1971
marondopari <mikaexo930@gmail.com>
2022-04-04 12:22:13 +0900

    scanfからscanf_sに変更

--- c001_InputNumber/Source.cpp
+++ c001_InputNumber/Source.cpp
@@ -1,11 +1,22 @@
 #include<stdio.h>
 
+//int main(void)
+//{
+//	int a;
+//
+//	printf("”š‚ğˆê‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+//	scanf("%d", &a);
+//	printf("%d‚ğ“ü—Í‚µ‚Ü‚µ‚½B\n", a);
+//
+//	return 0;
+//}
+
 int main(void)
 {
 	int a;
 
 	printf("”š‚ğˆê‚Â“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
-	scanf("%d", &a);
+	scanf_s("%d", &a);
 	printf("%d‚ğ“ü—Í‚µ‚Ü‚µ‚½B\n", a);
 
 	return 0;
