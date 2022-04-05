36baef906d7806d9dca58e2769da2c57f87d13b8
marondopari <mikaexo930@gmail.com>
2022-04-05 11:39:59 +0900

    合計表示プログラム

---
+++ c003AddNumber/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	printf("ˆê‚Â–Ú‚Ì”š‚ğ“ü—Í‚µ‚Ä‰º‚³‚¢\n");
+
+	scanf_s("%d",&a);
+
+	printf("“ñ‚Â–Ú‚Ì”š‚ğ“ü—Í‚µ‚Ä‰º‚³‚¢\n");
+
+	scanf_s("%d", &b);
+
+	printf("%d+%d=%d\n", a, b, a + b);
+
+	return 0;
+}
