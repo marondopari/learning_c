f8334d8ac0165632717aa25225a6ba79ede6e011
marondopari <mikaexo930@gmail.com>
2022-04-08 13:06:55 +0900

    コメント追加

--- c005AddNumberLoop/Source.cpp
+++ c005AddNumberLoop/Source.cpp
@@ -1,47 +1,21 @@
 #include<stdio.h>
 
-int main(void)
-{
-	int n;
-	int sum = 0;
-
-	while (1)
-	{
-		printf("数字を入力して下さい\n");
-		rewind(stdin);
-		scanf_s("%d", &n);
-
-		sum = sum + n;
-
-		int c = getchar();
-
-		if (c == '\n')
-		{
-			break;
-		}
-	}
-
-	printf("合計は%dです\n", sum);
-}
-
 //int main(void)
 //{
-//
 //	int n;
-//	int sum=0;
-//	char str[256];
-//	gets_s(str);
+//	int sum = 0;
 //
 //	while (1)
 //	{
-//
 //		printf("数字を入力して下さい\n");
 //		rewind(stdin);
-//		sscanf_s(str,"%d", &n);
+//		scanf_s("%d", &n);
 //
 //		sum = sum + n;
 //
-//		if (str[0] == 0)
+//		int c = getchar();
+//
+//		if (c == '\n')
 //		{
 //			break;
 //		}
@@ -49,6 +23,32 @@ int main(void)
 //
 //	printf("合計は%dです\n", sum);
 //}
+
+int main(void)
+{
+
+	int n;
+	int sum=0;
+	char str[256];
+	gets_s(str);
+
+	while (1)
+	{
+
+		printf("数字を入力して下さい\n");
+		rewind(stdin);
+		sscanf_s(str,"%d", &n);
+
+		sum = sum + n;
+
+		if (str[0] == 0)
+		{
+			break;
+		}
+	}
+
+	printf("合計は%dです\n", sum);
+}
 
 //int main(void)
 //{
@@ -73,3 +73,4 @@ int main(void)
 //
 //	printf("合計は%dです\n", sum);
 //}
+
