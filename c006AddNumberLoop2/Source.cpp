4b079518c3019383e7e1d6c2bca057df16141bd7
marondopari <mikaexo930@gmail.com>
2022-04-13 15:24:37 +0900

    コメント追加　define追加

--- c006AddNumberLoop2/Source.cpp
+++ c006AddNumberLoop2/Source.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define NUM 10		//10回
+
 
 //int main(void)
 //{
@@ -37,10 +39,11 @@ int main(void)
 {
 	int sum = 0;	//整数型の変数sumに０を代入
 	int n;				//整数型の変数nを用意する
-	int num[10];
-	int count=0;
+	int num[NUM];		//numという配列を宣言、要素数10
+	int count=0;	//整数型の変数countに０を代入
 
-	for (int i = 0; i < 10; i++)
+	//10回ループ
+	for (int i = 0; i < NUM; i++,count++)
 	{
 		printf("数字を入力して下さい\n");
 
@@ -54,14 +57,14 @@ int main(void)
 		}
 
 		sscanf_s(str, "%d", &n);	//文字列を整数に変換
-		num[i] = n;
+		num[i] = n;					//要素num[i]にｎを代入
 		sum = sum + n;				//累計
-		count = i+1;
 	}
 
+	//カウントの数だけループ
 	for (int i = 0; i < count; i++)
 	{
-		printf("%d+", num[i]);
+		printf("%d+", num[i]);		//配列の中身を順番に表示
 	}
 
 	printf("\n合計は%dです\n", sum);
