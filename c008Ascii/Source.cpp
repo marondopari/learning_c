6d97a4b944552cd4f56b461ef471b3abd761bc5e
marondopari <mikaexo930@gmail.com>
2022-04-15 11:57:37 +0900

    数字の41を文字として表示するプログラム

--- c008Ascii/Source.cpp
+++ c008Ascii/Source.cpp
@@ -2,14 +2,5 @@
 
 int main (void)
 {
-	int num = 0;		//整数型の変数numに０を代入
-	char text[256];		//textという配列を宣言、要素数256
-
-	//１２７回ループする
-	for (int i = 0; i <= 127; i++)
-	{
-		sprintf_s(text,"%d\n", num+i);	//数値を文字列に変換
-		printf("%s", text);				//文字列を出力する
-
-	}
+	printf("%c", 41);
 }
