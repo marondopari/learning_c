1786a2163a1a8c0231af390a877c0b66160430f4
marondopari <mikaexo930@gmail.com>
2022-04-19 15:45:08 +0900

    入力した文字が大文字か小文字か数字かを判定するプログラム

---
+++ c009AsciiCheck/Source.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char mozi;	//charŒ^‚Ì•Ï”
+	printf("1•¶Žš‚ð“ü—Í‚µ‚Ä‰º‚³‚¢\n");
+	scanf_s("%c", &mozi);	//•¶Žš‚Ì“ü—Í
+
+	//¬•¶Žš‚©”»’è
+	if (mozi >= 'a'&&mozi <= 'z')
+	{
+		printf("¬•¶Žš‚Å‚·\n");
+	}
+
+	//‘å•¶Žš‚©”»’è
+	if (mozi >= 'A'&&mozi <= 'Z')
+	{
+		printf("‘å•¶Žš‚Å‚·\n");
+	}
+
+	//”Žš‚©”»’è
+	if (mozi >= '0'&&mozi <= '9')
+	{
+		printf("”Žš‚Å‚·\n");
+	}
+}
