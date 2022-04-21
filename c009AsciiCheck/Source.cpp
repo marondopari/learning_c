8185df9613a70de52c10d5ee24962ed007f7bd0b
marondopari <mikaexo930@gmail.com>
2022-04-21 12:34:37 +0900

    入力した数から32を引いた値を返す関数追加 小文字の時に大文字を表示するプログラム

--- c009AsciiCheck/Source.cpp
+++ c009AsciiCheck/Source.cpp
@@ -1,17 +1,19 @@
 #include <stdio.h>
 int Add32(int a);	//プロトタイプ宣言
+int De32(int b);
 
 int main(void)
 {
 	char mozi;	//char型の変数
 	printf("1文字を入力して下さい\n");
 	scanf_s("%c", &mozi);	//文字の入力
-	printf("%c %d", mozi,mozi);
 	int n = Add32(mozi);
+	int b = De32(mozi);
 
 	//小文字か判定
 	if (mozi >= 'a'&&mozi <= 'z')
 	{
+		printf("%c %d", b, b);
 		printf("小文字です\n");
 	}
 
@@ -48,5 +50,11 @@ int main(void)
 int Add32(int a)
 {
 	int goukei= a + 32;	//32を足す
+	return goukei;
+}
+
+int De32(int c)
+{
+	int goukei = c - 32;	//32を足す
 	return goukei;
 }
