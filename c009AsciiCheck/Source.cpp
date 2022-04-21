8da0f6fc620941f87c75fa4dbaf216d50b62092d
marondopari <mikaexo930@gmail.com>
2022-04-21 10:41:32 +0900

    入力した文字が大文字の場合、文字に３２を足した数を画面に表示

--- c009AsciiCheck/Source.cpp
+++ c009AsciiCheck/Source.cpp
@@ -1,47 +1,47 @@
 #include <stdio.h>
-
-//int main(void)
-//{
-//	char mozi;	//char型の変数
-//	printf("1文字を入力して下さい\n");
-//	scanf_s("%c", &mozi);	//文字の入力
-//
-//	//小文字か判定
-//	if (mozi >= 'a'&&mozi <= 'z')
-//	{
-//		printf("小文字です\n");
-//	}
-//
-//	//大文字か判定
-//	else if (mozi >= 'A'&&mozi <= 'Z')
-//	{
-//		printf("大文字です\n");
-//	}
-//
-//	//数字か判定
-//	else if (mozi >= '0'&&mozi <= '9')
-//	{
-//		printf("数字です\n");
-//	}
-//
-//	else
-//	{
-//		printf("記号です");
-//	}
-//
-//}
-
-
 int Add32(int a);	//プロトタイプ宣言
 
 int main(void)
 {
-	int n=Add32(5);
+	char mozi;	//char型の変数
+	printf("1文字を入力して下さい\n");
+	scanf_s("%c", &mozi);	//文字の入力
+	int n = Add32(mozi);
 
-	printf("%d", n);
-	return 0;
+	//小文字か判定
+	if (mozi >= 'a'&&mozi <= 'z')
+	{
+		printf("小文字です\n");
+	}
+
+	//大文字か判定
+	else if (mozi >= 'A'&&mozi <= 'Z')
+	{
+		printf("%d", n);
+		printf("大文字です\n");
+	}
+
+	//数字か判定
+	else if (mozi >= '0'&&mozi <= '9')
+	{
+		printf("数字です\n");
+	}
+
+	else
+	{
+		printf("記号です");
+	}
 
 }
+
+//int main(void)
+//{
+//	int n=Add32(5);
+//
+//	printf("%d", n);
+//	return 0;
+//
+//}
 
 int Add32(int a)
 {
