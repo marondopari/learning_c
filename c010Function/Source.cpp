3c2909e7586d556b60e7bcd1c34519ecb85cba3d
marondopari <mikaexo930@gmail.com>
2022-04-22 12:50:46 +0900

    char型の引数をひとつとって、それが表示できない文字だったらreturnでぬけて、表示できる文字だったらprintfで文字として表示する関数

--- c010Function/Source.cpp
+++ c010Function/Source.cpp
@@ -2,12 +2,18 @@
 
 void printHelloWorld(void);
 void printNumber(int a);
+void printChar(char b);
 
 int main(void)
 {
 	printHelloWorld();
 
 	printNumber(10);
+
+	int mozi;
+	printf("\n•¶Žš‚ð“ü—Í‚µ‚Ä‰º‚³‚¢\n");
+	scanf_s("%d", &mozi);
+	printChar(mozi);
 
 	return 0;
 }
@@ -21,4 +27,16 @@ void printHelloWorld(void)
 void printNumber(int a)
 {
 	printf("%d",a);
+}
+
+void printChar(char b)
+{
+	if (b >= 32&&b <= 126)
+	{
+		printf("%c", b);
+	}
+	else
+	{
+		return;
+	}
 }
