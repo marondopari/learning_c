932b96a5d01b7c0d7c8313d864481463550b7a8a
marondopari <mikaexo930@gmail.com>
2022-04-22 10:44:40 +0900

    intの引数を一つ取って、それを%dでprintfする関数追加

--- c010Function/Source.cpp
+++ c010Function/Source.cpp
@@ -1,13 +1,25 @@
 #include<stdio.h>
 
 void printHelloWorld(void);
+void print(int a);
 
 int main(void)
 {
 	printHelloWorld();
+
+	print(10);
+
+	return 0;
 }
 
 void printHelloWorld(void)
 {
-	printf("HelloWorld");
+	printf("HelloWorld\n");
+	return;
+}
+
+void print(int a)
+{
+	printf("%d",a);
+	return;
 }
