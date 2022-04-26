17249634205a87ca358b63f3098688e68f2092d7
marondopari <mikaexo930@gmail.com>
2022-04-26 10:47:13 +0900

    から13までのランダムな数を返す関数をつくって、mainで呼ぶ

---
+++ c011BlackJack/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+void randam(void);
+
+int main(void)
+{
+	randam();		//関数を呼ぶ
+}
+
+void randam(void)
+{
+	srand((unsigned)time(NULL));	////乱数の種に現在の時刻を指定
+	int a;		//整数型の変数aを用意
+	a=rand() % 13 + 1;	//1~13のランダムな数値
+	printf("%d\n", a);	//aの値を出力
+}
