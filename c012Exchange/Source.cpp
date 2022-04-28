4edda954460932e8b9b02569bf0f958c8eaaa524
marondopari <mikaexo930@gmail.com>
2022-04-28 10:16:38 +0900

    ドルを円に変換する関数と、円をドルに変換する関数追加

---
+++ c012Exchange/Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void en(int a);
+void dol(int b);
+
+int main(void)
+{
+
+}
+//ƒhƒ‹‚ğ‰~‚É•ÏŠ·‚·‚éŠÖ”
+void en(int a)
+{
+	a*128.87;
+}
+
+//‰~‚ğƒhƒ‹‚É•ÏŠ·‚·‚éŠÖ”
+void dol(int b)
+{
+	b / 128.87;
+}
