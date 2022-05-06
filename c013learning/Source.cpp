b38c47dc7466fbebc343dddfed3c3e12198ce68e
marondopari <mikaexo930@gmail.com>
2022-05-06 12:38:09 +0900

    関数名前変更 printAddとcalcAndPrintAddの中で、 calcAddを呼ぶ

--- c013learning/Source.cpp
+++ c013learning/Source.cpp
@@ -2,39 +2,39 @@
 #include<stdlib.h>
 #include <time.h>
 
-int add(int v, int q);
-void printNumber(int a,int b);
-int printNumberReturn(int k, int l);
+int calcAdd(int v, int q);
+void printAdd(int a,int b);
+int calcAndPrintAdd(int k, int l);
 
 int main(void)
 {
 	int f = 20;
 	int h = 4;
 
-	int y=add(f, h);
+	int y= calcAdd(f, h);
 	printf("%dです\n", y);
 
-	printNumber(f, h);
-	printNumberReturn(f, h);
+	printAdd(f, h);
+	calcAndPrintAdd(f, h);
 }
 
 //二つの数の合計を返す関数
-int add(int v, int q)
+int calcAdd(int v, int q)
 {
 	int goukei = v + q;
 	return goukei;
 }
 
 //表示する関数
-void printNumber(int a,int b)
+void printAdd(int a,int b)
 {
-	printf("\n%d+%d=%d", a,b,a+b);
+	printf("\n%d+%d=%d", a,b, calcAdd(a,b));
 }
 
 //表示する関数合計の値を返り値として返す関数
-int printNumberReturn(int k, int l)
+int calcAndPrintAdd(int k, int l)
 {
-	printf("\n%d+%d=%d", k, l, k + l);
+	printf("\n%d+%d=%d", k, l, calcAdd(k, l));
 	int gokei = k + l;
 	return gokei;
 }
