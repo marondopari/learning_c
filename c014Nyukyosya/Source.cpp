ac6927136edc32029a76d2f3d43b650f694423c9
marondopari <mikaexo930@gmail.com>
2022-05-13 16:48:33 +0900

    入居者登録の処理for文追加

--- c014Nyukyosya/Source.cpp
+++ c014Nyukyosya/Source.cpp
@@ -1,15 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define JUMIN 10		//住民10
 
 int main(void)
 {
-	char str[256];
+	char jumin[JUMIN][256];	//最大256文字の名前を10個格納する配列
+	int count = 0;
 
-	printf_s("入居者を入力して下さい\n");
+	for (int i = 0; i < JUMIN; i++,count++)
+	{
+		printf_s("入居者を入力して下さい\n");
 
-	scanf_s("%s", &str, sizeof(str));
+		//空エンターだったら
+		if (jumin[0] == 0)
+		{
+			break;			//ループから抜ける
+		}
+
+		scanf_s("%s", &jumin[i], sizeof(jumin[i]));
+	}
 
 	printf_s("入居者は\n");
-	printf_s("%s\n", &str);
+
+	for (int i = 0; i < count; i++)
+	{
+			printf_s("%s\n", &jumin[i]);
+	}
+
 	printf_s("です");
+
 }
